b3d6a6c9a230cd7498844788e72f5eca1f2d878c
Samuel-Henrique12 <sdikdplayd@gmail.com>
2025-08-22 14:56:24 -0300

    Métodos para Utilização de API

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,61 +7,68 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __construct(Marca $marca)
     {
-        $marcas = Marca::all();
-        return $marcas;
+        $this->marca = $marca;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    public function index()
+    {
+        //$marcas = Marca::all();
+        $marcas = $this->marca->all();
+        return response()->json($marcas,200);
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
-        return 'tamo ai';
+        //$marca = Marca::create($request->all());
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+        //dd($request->file('imagem'));
+        $image = $request->file('imagem');
+        $image->store('imagens');
+        dd('Upload de Arquivos');
+        //$marca = $this->marca->create($request->all());
+        return response()->json($marca,201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Marca $marca)
+    public function show($id)
     {
-       return $marca;
+       $marca = $this->marca->find($id);
+       if($marca === null) {
+           return response()->json(['erro' => 'Ta nulo irmão'],404);
+       }
+        return response()->json($marca, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Marca $marca)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if($marca === null) {
+            return response()->json(['erro' => 'Ta nulo'], 404);
+        }
+        $request->validate($marca->rules(), $marca->feedback());
+        $marca->update($request->all());
+        return response()->json($marca, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if($marca === null) {
+            return response()->json(['erro' => 'Ta nulo'], 404);
+        }
+        $marca->delete();
+        return response()->json(['msg' => 'RE MO VI DO!'], 200);
     }
 }
--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -9,4 +9,18 @@ class Marca extends Model
 {
     use HasFactory;
     protected $fillable = ['nome' , 'imagem'];
+
+    public function rules() {
+        return [
+            'nome' => 'required|unique:marcas|min:3',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedback() {
+        return [
+            'required' => 'O campo é obrigatorio',
+            'nome.unique' => 'O nome está em uso'
+        ];
+    }
 }
